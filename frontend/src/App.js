e52c0f4dd451109896df3a568346362f8d39473f
Edward <46017382+echang49@users.noreply.github.com>
2020-10-24 13:37:07 -0400

    Repository Initialization

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+ } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      </Router>
+    );
+  }
+}
+
+export default App;
